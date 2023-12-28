82e4b9fddd4c954fb737b5f46145c94fb8978020
GekiteniusY <genkichi.yogi@gmail.com>
2023-12-28 17:13:20 +0900

    propsの使い方

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
 import logo from './logo.svg';
 import './App.css';
+import Article from './component/Article';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+    <>
+        <div>
+            <Article
+                title={'新・日本一わかりやすいReact入門1'}
+                content={'強のトピックはpropsについて'}
+            />
+            <Article
+                title={'新・日本一わかりやすいReact入門2'}
+                content={'強のトピックはpropsについて'}
+            />
+            <Article
+                title={'新・日本一わかりやすいReact入門3'}
+                content={'強のトピックはpropsについて'}
+            />
+        </div>
+    </>
+    );
 }
+
+
+
+
+//function App() {
+//  return (
+//    <div className="App">
+//      <header className="App-header">
+//        <img src={logo} className="App-logo" alt="logo" />
+//        <p>
+//          Edit <code>src/App.js</code> and save to reload.
+//        </p>
+//        <a
+//          className="App-link"
+//          href="https://reactjs.org"
+//          target="_blank"
+//          rel="noopener noreferrer"
+//        >
+//          Learn React
+//        </a>
+//      </header>
+//    </div>
+//  );
+//}
 
 export default App;
